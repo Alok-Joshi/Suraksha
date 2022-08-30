478b878ae22c565d931c806d433d74b379a45d33
Alok Joshi <AlokJoshi523@gmail.com>
2022-08-30 23:12:07 +0530

    initial implementation of gps map, in progress

--- src/Suraksha/gps_map/views.py
+++ src/Suraksha/gps_map/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_map(request):
+    """ Renders the map. Login is required """
+    return render(request,"gps_map/map.html")
+    if(request.user.is_authenticated):
+        #get the devices linked to user, pass them as context to template, render template
+        pass
+
+    else:
+        #redirect to the login page 
+        pass
