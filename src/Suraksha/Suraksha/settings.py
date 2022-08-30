a4553c7b92dd9e2213506e3e45b1e0450a89e9f9
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-08-30 15:11:50 +0530

    added apps in installed apps list and added the respective urls

--- src/Suraksha/Suraksha/settings.py
+++ src/Suraksha/Suraksha/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'data_streamer',
+    'device_auth',
+    'gps_map',
+    'login',
+    'register',
 ]
 
 MIDDLEWARE = [
--- src/Suraksha/Suraksha/urls.py
+++ src/Suraksha/Suraksha/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, data_streamer, device_auth, gps_map, login, register
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data_streamer', data_streamer.urls),
+    path('register', register.urls),
+    path('login', login.urls),
+    path('device_auth', device_auth.urls),
+    path('gps_map', gps_map.urls),
+
 ]
