3c6787e640ed066dea94878262ad646e84fee73c
Alok Joshi <AlokJoshi523@gmail.com>
2022-09-15 10:07:14 +0530

    Basic Implementation of redis publisher done.

--- src/redis_publisher/publisher.py
+++ src/redis_publisher/publisher.py
@@ -1,5 +1,12 @@
 import sys
+import time
+import aioredis
+import asyncio
 
+
+REDIS_URL = "redis://127.0.0.1:6379" 
+
+#TODO: Remove this hardcoding and take the redis url from environment variables
 
 help_message = """
                   Usage: python3 publisher.py path_to_gps_file redis_channel_name time
@@ -7,7 +14,7 @@ help_message = """
                   example: python3 publisher.py gps_dat.txt gps_dev_23  2
                   """
                 
-def main():
+async def main():
 
     if(len(sys.argv)>1 and sys.argv[1] == 'help'):
         print(help_message)
@@ -15,27 +22,29 @@ def main():
     elif(len(sys.argv) < 4):
         print("Incorrect Number of arguments. Use publisher.py help to see usage ")
         return;
-
-    
     else:
-        [ 
+        try:
+            file_path = sys.argv[1]
+            channel_name = sys.argv[2]
+            sleep_time = int(sys.argv[3])
 
+            redis_client = aioredis.from_url(REDIS_URL)
+            gps_coordinates_file = open(file_path,"r")
+            gps_coordinates = list(gps_coordinates_file.readlines())
 
+            for coords in gps_coordinates:
+                await redis_client.publish(channel_name,coords)
+                if(sleep_time>0):
+                    time.sleep(sleep_time)
 
-
-
-
-
-
-
-
-
+        except Exception as e:
+            print(e)
 
 
 
 
 if(__name__ == "__main__"):
-        main()
+        asyncio.run(main())
 
 
 
