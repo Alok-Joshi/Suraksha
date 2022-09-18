75d9d6181400888d6f87405a403e47dbfe6e4c48
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-09-18 18:58:01 +0530

    added a basic class based view to device auth

--- src/Suraksha/device_auth/urls.py
+++ src/Suraksha/device_auth/urls.py
@@ -1,3 +1,7 @@
 from django.urls import include, path
+from .views import Authenticate
 
-urlpatterns = []
+app_name = 'authenticate'
+urlpatterns = [
+    path('authenticate/', Authenticate.as_view(), name = 'authenticate'),
+]
--- src/Suraksha/device_auth/views.py
+++ src/Suraksha/device_auth/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class Authenticate(TemplateView):
+    def get(self, request):
+        return render(request, "device_auth/auth.html")
