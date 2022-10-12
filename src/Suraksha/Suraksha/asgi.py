188a5e88799d6d845402b4e535f9efa0d1d85a8c
Alok Joshi <AlokJoshi523@gmail.com>
2022-10-12 08:44:30 +0530

    Changed import statement for data streamer

--- src/Suraksha/Suraksha/asgi.py
+++ src/Suraksha/Suraksha/asgi.py
@@ -12,15 +12,14 @@ from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import data_streamer
-
+from data_streamer.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Suraksha.settings')
 
 application = ProtocolTypeRouter({
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 URLRouter(
-                    data_streamer.routing.websocket_urlpatterns
+                    websocket_urlpatterns
                     )
                 )
             ),
